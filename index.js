268f6840a73a0f1862e8e3f015f66e1a40ea63c8
mach666 <officialyadavprince@gmail.com>
2021-08-03 15:10:51 +0530

    'V1.5'

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const PORT=process.env.PORT||8000
 APP.get('/',(req,res)=>{
     res.send("This is get request at CI&CD implmentation with CD automated via HEROKU after gui approach")
 })
+APP.get('/post',(req,res)=>{
+    res.send("This is second api call")
+})
 
 APP.listen(PORT,()=>{
     console.log(`Server is listeninig on port:${PORT}`)
