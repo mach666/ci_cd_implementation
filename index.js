507e568cdaa96d751c2ea9b10e2521904e8e7701
mach666 <officialyadavprince@gmail.com>
2021-08-03 13:30:14 +0530

    'V1.6'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const APP=EXPRESS()
 const PORT=process.env.PORT||8000
 
 APP.get('/',(req,res)=>{
-    res.send("This is get request at CI&CD implmentation")
+    res.send("This is get request at CI&CD implmentation with CD automated")
 })
 
 APP.listen(PORT,()=>{
