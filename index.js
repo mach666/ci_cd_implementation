704206832752314392df533450b4db3024fe3e60
mach666 <officialyadavprince@gmail.com>
2021-08-03 17:01:42 +0530

    'V3.1'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const APP=EXPRESS()
 const PORT=process.env.PORT||8000
 
 APP.get('/',(req,res)=>{
-    res.send("This is get request at CI&CD implmentation with CD automated via HEROKU after programming approach")
+    res.send("This is get request at CI&CD implmentation with CD automated via HEROKU after programming")
 })
 APP.get('/post',(req,res)=>{
     res.send("This is second api call")
