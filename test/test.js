13eadf105e1ddfec105104628898646ec1bfc52d
mach666 <officialyadavprince@gmail.com>
2021-09-16 00:07:55 +0530

    you

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ const app = require("../index");
 
 describe("GET /", () => {
     it("responds with Hello World!", (done) => {
-        request(app).get("/").expect("This is get request at CI&CD implmentation with CD automated via HEROKU after programming", done);
+        request(app).get("/").expect("Thi is get request at CI&CD implmentation with CD automated via HEROKU after programming", done);
     })
 });
